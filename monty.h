4dead139d4cf18971bd9ab387291dd142d471900
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-16 16:29:17 +0100

    added header file

---
+++ monty.h
@@ -0,0 +1,14 @@
+#ifndef monty_h
+#define monty_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+#include <ctype.h>
+
+/**
+ * struct stack_s
+
+#endif
