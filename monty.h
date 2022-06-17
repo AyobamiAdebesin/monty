315e257a599cff0335f4bf448ed23ceb831e70b1
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-17 14:16:48 +0100

    Updated header file

--- monty.h
+++ monty.h
@@ -9,6 +9,37 @@
 #include <ctype.h>
 
 /**
- * struct stack_s
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node stucture
+ * for stack, queues, LIFO, FIFO
+ */
+
+typedef struct stack_s
+{
+  int n;
+  struct stack_s *prev;
+  struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcoode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+
+
 
 #endif
